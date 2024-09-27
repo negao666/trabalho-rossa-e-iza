e48b1abe42e966367a187c27f70e3dae511cf899
negao666 <gabrielrossa336@gmail.com>
2024-09-27 08:43:30 -0300

    Add files via upload

---
+++ Aula04_loops_Izadora_Rossa-JS-main/script.js
@@ -0,0 +1,16 @@
+//laço while = enquanto
+
+var x = 0;
+document.write('<br>Testando com o while');
+while(x<=10){
+    document.write('<br>O valor de x é: '+x );
+    x=x+1;
+}
+
+//laço for = para
+
+document.write('<br>Testando com o for');
+while(x<=10){
+    document.write('<br>O valor de x é: '+x);
+    x=x+1;
+}
