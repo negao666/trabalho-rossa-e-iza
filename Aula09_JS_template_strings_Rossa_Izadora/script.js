a5633b58ac3960c30810f70a27e424687904ca87
negao666 <gabrielrossa336@gmail.com>
2024-10-28 08:37:43 -0300

    Add files via upload

---
+++ Aula09_JS_template_strings_Rossa_Izadora/script.js
@@ -0,0 +1,10 @@
+let nome = "Izadora";
+let sobrenome = "Rodrigues";
+let idade = 16;
+
+let mensagem = "Meu nome é " +nome+ " " +sobrenome+" e minha idade é "+idade+" anos";
+console.log(mensagem);
+
+let mensagem2 = `Meu nome é ${nome} ${sobrenome} e minha idade é ${idade} anos`;
+console.log(mensagem2);
+
