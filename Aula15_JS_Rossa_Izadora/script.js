e0f9cd5a3ba45acf9f91060f8db77c2a7ac0239f
negao666 <gabrielrossa336@gmail.com>
2024-11-25 08:57:02 -0300

    Add files via upload

---
+++ Aula15_JS_Rossa_Izadora/script.js
@@ -0,0 +1,17 @@
+// 3 metodos facilitadores: includes, starwith e endwith
+
+let nomes = ["Amir","Bemir","Cenir"]
+console.log (nomes)
+console.log(nomes.includes("Bemir"))
+
+if (nomes.incluides("Amir")){[
+   console.log("O meliante Amir está incluido em nosso array")
+]}
+else{
+console.log("O meliante Amir não está no array")
+}
+
+let aluno= "Albano"
+console.log(aluno.startsWith("Alb"))
+console.log(aluno.endsWith("ano"))
+
